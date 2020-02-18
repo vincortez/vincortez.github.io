c6d136c6e6d0ea0ded6e2663381c3fef81b55e93
Vin <vincortezstudio@gmail.com>
2020-02-18 18:34:27 +0800

    Version 0.0.0

---
+++ precache-manifest.779212ea58f428fb5d099addb075495f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "353e1d92d52176e20774af426c4453b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a792d8e841ba72992ec5",
+    "url": "/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "eb376f5a2a75f2f2304e",
+    "url": "/static/css/main.b42b3cfe.chunk.css"
+  },
+  {
+    "revision": "a792d8e841ba72992ec5",
+    "url": "/static/js/2.c33a417a.chunk.js"
+  },
+  {
+    "revision": "4fe47f505a86365e7928e2b6e494a0c5",
+    "url": "/static/js/2.c33a417a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb376f5a2a75f2f2304e",
+    "url": "/static/js/main.a174c305.chunk.js"
+  },
+  {
+    "revision": "7093958f848a6f4d11a7",
+    "url": "/static/js/runtime-main.7e6e0418.js"
+  }
+]);
